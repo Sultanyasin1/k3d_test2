7ae1d6a8df3156ef8d476ec0e7abc50d7868bbcc
sultanyasin1 <sultan.yasin@ekerit.se>
2026-04-23 11:51:20 +0200

    adding new route

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TestRoute2() {
+  return (
+    <div>
+      Hello, this is a TestRoute2
+      5555555555555555555544444444444444666666666666666
+    </div>
+  );
+}
+
+export default TestRoute2;
